2ba1d78f8ca26fea044ff3553e564f052d727d7d
Aniket Jadhav <jadhavaniket0118@gmail.com>
2023-05-03 21:56:00 +0530

    final chat system

--- discord-clone-backend/controllers/friendInvitations/friendInvitationControllrs.js
+++
@@ -1,5 +0,0 @@
-const postInvite =require('./postInvite');
-
-exports.controllers={
-postInvite,
-};
--- discord-clone-backend/socketHandlers/disconnectHandler.js
+++
@@ -1,9 +0,0 @@
-//const { Socket } = require('socket.io');
-const serverStore= require('../serverStore');
-const disconnectHandller=(socket)=>{
-    serverStore.removeConnectedUser(socket.id);
-}; 
-
-
-
-module.exports= disconnectHandller;
